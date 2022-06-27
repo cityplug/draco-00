9d65aa1d3e0c22326249a3cae6f8388d81fdd7f7
cityplug <seni@cityplug.co.uk>
2022-06-27 21:31:33 +0100

    .

--- .scripts/finish.sh
+++ .scripts/finish.sh
@@ -14,6 +14,9 @@ net.ipv4.ip_forward = 1
 net.ipv6.conf.all.forwarding = 1" >> /etc/sysctl.conf
 sysctl -p
 
+mkdir -p /draco/storage/swap
+chown -R shay /draco/storage/swap
+
 echo "# --- Enter pihole user password --- #"
 docker exec -it pihole pihole -a -p
 echo "#  ---  COMPLETED | REBOOT SYSTEM  ---  #"
